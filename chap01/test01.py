78f4a0aa927aa006cfe3c422933d4dd3f99c0954
746861631@qq.com <746861631@qq.com>
2021-01-28 22:43:20 +0800

    修改代码

--- chap01/test01.py
+++ chap01/test01.py
@@ -1,4 +1,6 @@
 import cv2                                                     #导入OpenCV模块
-img_lena1 = cv2.imread(r'..\OriginalImage\lena.jpg')                    #将图片分别读入img1,img2,img3中
-img_lena2 = cv2.imread(r'..\OriginalImage\lena.jpg', cv2.IMREAD_GRAYSCALE)
-print(type(img_lena1), type(img_lena2))
+img_lena1 = cv2.imread(r'..\OImage\lena.jpg')                    #将图片分别读入img1,img2,img3中
+img_lena2 = cv2.imread(r'..\OImage\lena.jpg', cv2.IMREAD_GRAYSCALE)
+img_lena3 = cv2.imread(r'..\OImage\lena.bmp')
+img_lena4 = cv2.imread(r'..\OImage\lena.bmp', cv2.IMREAD_GRAYSCALE)
+print(type(img_lena1), type(img_lena2), type(img_lena3), type(img_lena4))
